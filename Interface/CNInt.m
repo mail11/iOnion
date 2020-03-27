c68c78fc51cd9131ab9dba8d689de1e704764fd2
Liam Yasin <liam.yasin15@imperial.ac.uk>
2020-03-27 16:44:00 +0000

    Changed script to be a function, implemented developer mode,

--- Interface/CNInt.m
+++ Interface/CNInt.m
@@ -2,87 +2,237 @@ function C = CNInt(Dk)
 
 %% Define parameters
 
-global data;
-int_width=0;% Define the width of the interface region (0 is the default value)
+% Set up the global variables
 
-% Experimental setup
-Duration=.083; % Time of exchange in hours (found with Kiloran correction)
-L_1 = 0.3; % Length of LSCF [um]
-L_2 = 0.7; % Length of GDC [um]
+global data;
+global L_1;
+global L_2;
+global L_3;
+global steps_x;
+global answer;
+global Duration;
+
+int_width=0;% Define the width of the interface region (0 is the default value)
 
 % Simulation parameters
 delta_t=2; % Define the time step [s] (this should be a derived parameter based on max value of sigma)
 
-% Derived properties
-%D_int=2*(r/delta_x+1/D_LSCF+1/D_GDC)^-1; % Define the diffusivity of the interface [um^2/s]
-Length=L_1+L_2; % Total sample length [um]
-delta_x=Length/(length(data)-1); % Define the spatial step [um]
-steps_x=Length/delta_x+1; % Define number of spatial nodes
-steps_t=Duration*3600/delta_t+1; % Define number of time steps
-x=0:delta_x:Length; % Define the length of the profile in steps of delta_x
-sigmaLSCF=Dk(1)*(delta_t)/(2*delta_x^2); % Calculate sigma of LSCF
-sigmaGDC=Dk(2)*(delta_t)/(2*delta_x^2); % Calculate sigma of GDC
-sigma_int=Dk(3)*(delta_t)/(2*delta_x^2);% Calculate sigma of the first interface
-h=Dk(4)/Dk(1); %Calculate h
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Build simulation components
-% Set up a sub-matrix L which contains the information regarding the surface exchange as well as diffusion through the LSCF layer
-Layer1 = full(gallery('tridiag',round(L_1/delta_x),sigmaLSCF,1-2*sigmaLSCF,sigmaLSCF)); % Bulk diffusion parameters
-Layer1(1,1) = 1-2*sigmaLSCF*(1+delta_x*h); %Surface boundary condition
-Layer1(1,2) = 2*sigmaLSCF; %Surface boundary condition
-
-% Set up a sub-matrix E for the bulk diffusion properties of the electrolyte
-Layer2 = full(gallery('tridiag',round((L_2-delta_x)/delta_x)+2,sigmaGDC,1-2*sigmaGDC,sigmaGDC)); %Bulk diffusion parameters
-Layer2(end,end) = 1+sigmaGDC; %Mirror boundary condition parameters
-Layer2(end,end-1) = -2*sigmaGDC;
-Layer2(end,end-2) = sigmaGDC;
-
-% Set up the main matrix which combines L, E and interface properties into one
-A = full(gallery('tridiag',round(steps_x),sigmaGDC,1-2*sigmaGDC,sigmaGDC)); % Create a tridiagonal basis using the electrolyte values
-A(1:L_1/delta_x,1:L_1/delta_x)=Layer1; %integrate the sub-matrix L into the main matrix
-A(round((Length-L_2)/delta_x)+1:end,round((Length-L_2)/delta_x)+1:end)=Layer2; %integrate the sub-matrix E into the main matrix
-
-%Set up the part of the matrix at the interface with varying width
-for l=L_1/delta_x:L_1/delta_x+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
-    A(l,l-1) = sigma_int;
-    A(l,l) = 1-2*sigma_int;
-    A(l,l+1) = sigma_int;
+if str2double(answer(2)) == 1
+    
+    %% One layer
+    
+    Length=L_1; % Total sample length [um]
+    delta_x=Length/(length(data)-1); % Define the spatial step [um]
+    steps_x=Length/delta_x+1; % Define number of spatial nodes
+    steps_t=Duration*3600/delta_t+1; % Define number of time steps
+    x=0:delta_x:Length; % Define the length of the profile in steps of delta_x
+    sigma_1=Dk(1)*(delta_t)/(2*delta_x^2); % Calculate sigma of LSCF
+    h=Dk(2)/Dk(1); %Calculate h
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Build simulation components
+    % Set up a sub-matrix L which contains the information regarding the surface exchange as well as diffusion through the LSCF layer
+    A = full(gallery('tridiag',round(steps_x),sigma_1,1-2*sigma_1,sigma_1)); % Create a tridiagonal basis using the electrolyte values
+    A(1,1) = 1-2*sigma_1*(1+delta_x*h); %Surface boundary condition
+    A(1,2) = 2*sigma_1; %Surface boundary condition
+    A(end,end) = 1+sigma_1; %Mirror boundary condition parameters
+    A(end,end-1) = -2*sigma_1;
+    A(end,end-2) = sigma_1;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %Setting up the transformation matrix for the next timestep A(n+1), which
+    %is just the same as the previous matrix but with flipped signs.
+    
+    A_n = full(gallery('tridiag',round(steps_x),-sigma_1,1+2*sigma_1,-sigma_1));
+    A_n(1,1) = 1+2*sigma_1*(1+delta_x*h);
+    A_n(1,2) = -2*sigma_1;
+    A_n(end,end) = 1-sigma_1;
+    A_n(end,end-1) = 2*sigma_1;
+    A_n(end,end-2) = -sigma_1;
+    
+elseif str2double(answer(2)) == 2 % If there are two layers, fminsearch will call upon the following code which is written for a matrix containing two layers
+    %% Two layers
+    
+    % Derived properties
+    %D_int=2*(r/delta_x+1/D_LSCF+1/D_GDC)^-1; % Define the diffusivity of the interface [um^2/s]
+    Length=L_1+L_2; % Total sample length [um]
+    delta_x=Length/(length(data)-1); % Define the spatial step [um]
+    steps_x=Length/delta_x+1; % Define number of spatial nodes
+    steps_t=Duration*3600/delta_t+1; % Define number of time steps
+    x=0:delta_x:Length; % Define the length of the profile in steps of delta_x
+    sigma_1=Dk(1)*(delta_t)/(2*delta_x^2); % Calculate sigma of LSCF
+    sigma_2=Dk(2)*(delta_t)/(2*delta_x^2); % Calculate sigma of GDC
+    sigma_int=Dk(3)*(delta_t)/(2*delta_x^2);% Calculate sigma of the first interface
+    h=Dk(4)/Dk(1); %Calculate h
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Build simulation components
+    % Set up a sub-matrix L which contains the information regarding the surface exchange as well as diffusion through the LSCF layer
+    Layer1 = full(gallery('tridiag',round(L_1/delta_x),sigma_1,1-2*sigma_1,sigma_1)); % Bulk diffusion parameters
+    Layer1(1,1) = 1-2*sigma_1*(1+delta_x*h); %Surface boundary condition
+    Layer1(1,2) = 2*sigma_1; %Surface boundary condition
+    
+    % Set up a sub-matrix E for the bulk diffusion properties of the electrolyte
+    Layer2 = full(gallery('tridiag',round((L_2-delta_x)/delta_x)+2,sigma_2,1-2*sigma_2,sigma_2)); %Bulk diffusion parameters
+    Layer2(end,end) = 1+sigma_2; %Mirror boundary condition parameters
+    Layer2(end,end-1) = -2*sigma_2;
+    Layer2(end,end-2) = sigma_2;
+    
+    % Set up the main matrix which combines L, E and interface properties into one
+    A = full(gallery('tridiag',round(steps_x),sigma_2,1-2*sigma_2,sigma_2)); % Create a tridiagonal basis using the electrolyte values
+    A(1:round(L_1/delta_x),1:round(L_1/delta_x))=Layer1; %integrate the sub-matrix L into the main matrix
+    A(round((Length-L_2)/delta_x)+1:end,round((Length-L_2)/delta_x)+1:end)=Layer2; %integrate the sub-matrix E into the main matrix
+    
+    %Set up the part of the matrix at the interface with varying width
+    for l=round(L_1/delta_x):round(L_1/delta_x)+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
+        A(l,l-1) = sigma_int;
+        A(l,l) = 1-2*sigma_int;
+        A(l,l+1) = sigma_int;
+    end
+    
+    A(round(L_1/delta_x)+int_width,round(L_1/delta_x)+1+int_width) = sigma_2;
+    A(round(L_1/delta_x)+1+int_width,round(L_1/delta_x)+int_width) = sigma_int; % Manual modification to ensure the matrix is set up correctly
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %Setting up the transformation matrix for the next timestep A(n+1), which
+    %is just the same as the previous matrix but with flipped signs.
+    
+    Layer1_n = full(gallery('tridiag',round(L_1/delta_x),-sigma_1,1+2*sigma_1,-sigma_1));
+    Layer1_n(1,1) = 1+2*sigma_1*(1+delta_x*h);
+    Layer1_n(1,2) = -2*sigma_1;
+    
+    Layer2_n = full(gallery('tridiag',round((L_2-delta_x)/delta_x)+2,-sigma_2,1+2*sigma_2,-sigma_2));
+    Layer2_n(end,end) = 1-sigma_2;
+    Layer2_n(end,end-1) = 2*sigma_2;
+    Layer2_n(end,end-2) = -sigma_2;
+    
+    A_n = full(gallery('tridiag',round(steps_x),-sigma_2,1+2*sigma_2,-sigma_2));
+    A_n(1:round(L_1/delta_x),1:round(L_1/delta_x))=Layer1_n;
+    A_n(round((Length-L_2)/delta_x)+1:end,round((Length-L_2)/delta_x)+1:end)=Layer2_n;
+    
+    for l=round(L_1/delta_x):round((L_1+int_width)/delta_x)
+        A_n(l,l-1) = -sigma_int;
+        A_n(l,l) = 1+2*sigma_int;
+        A_n(l,l+1) = -sigma_int;
+    end
+    
+    A_n(round(L_1/delta_x)+int_width,round(L_1/delta_x)+1+int_width) = -sigma_2;
+    A_n(round(L_1/delta_x)+1+int_width,round(L_1/delta_x)+int_width) = -sigma_int;
+    
+elseif str2double(answer(2)) == 3 % Set up a matric calculation with three layers
+    
+    %% Three layers
+    
+    % Derived properties
+    %D_int=2*(r/delta_x+1/Dk(1)+1/Dk(2))^-1; % Define the diffusivity of the interface [um^2/s]
+    Length=L_1+L_2+L_3; % Total sample length [um]
+    delta_x=Length/(length(data)-1);
+    steps_x=Length/delta_x+1; % Define number of spatial nodes
+    steps_t=Duration*3600/delta_t+1; % Define number of time steps
+    x=0:delta_x:Length; % Define the length of the profile in steps of delta_x
+    sigma_1=Dk(1)*(delta_t)/(2*delta_x^2); % Calculate sigma of LSCF
+    sigma_2=Dk(2)*(delta_t)/(2*delta_x^2); % Calculate sigma of GDC
+    sigma_3=Dk(3)*(delta_t)/(2*delta_x^2); % Calculate sigma of YSZ
+    sigma_int1=Dk(4)*(delta_t)/(2*delta_x^2);% Calculate sigma of the first interface
+    sigma_int2=Dk(5)*(delta_t)/(2*delta_x^2);% Calculate sigma of the second interface
+    h=Dk(6)/Dk(1); %Calculate h
+    
+    
+    %% Build simulation components
+    % Set up a sub-matrix L which contains the information regarding the surface exchange as well as diffusion through the LSCF layer
+    Layer1 = full(gallery('tridiag',round(L_1/delta_x),sigma_1,1-2*sigma_1,sigma_1)); % Bulk diffusion parameters
+    Layer1(1,1) = 1-2*sigma_1*(1+delta_x*h); %Surface boundary condition
+    Layer1(1,2) = 2*sigma_1; %Surface boundary condition
+    
+    % Set up a sub-matrix E for the bulk diffusion properties of the electrolyte
+    Layer2 = full(gallery('tridiag',round((L_2-delta_x)/delta_x)+1,sigma_2,1-2*sigma_2,sigma_2)); %Bulk diffusion parameters
+    
+    % Set up a sub-matrix S for the bulk diffusion properties of the substrate
+    Layer3 = full(gallery('tridiag',round((L_3-delta_x)/delta_x)+2,sigma_3,1-2*sigma_3,sigma_3)); %Bulk diffusion parameters
+    Layer3(end,end) = 1+sigma_3; %Mirror boundary condition parameters
+    Layer3(end,end-1) = -2*sigma_3;
+    Layer3(end,end-2) = sigma_3;
+    
+    % Set up the main matrix which combines L, E, S and interface properties into one
+    A = full(gallery('tridiag',round(steps_x),sigma_2,1-2*sigma_2,sigma_2)); % Create a tridiagonal basis using the electrolyte values
+    A(1:round(L_1/delta_x),1:round(L_1/delta_x))=Layer1; %integrate the sub-matrix L into the main matrix
+    A(1+round(L_1/delta_x):round((L_2+L_1)/delta_x),1+round(L_1/delta_x):round((L_2+L_1)/delta_x))=Layer2; %integrate the sub-matrix E into the main matrix
+    A(round((Length-L_3)/delta_x)+1:end,round((Length-L_3)/delta_x)+1:end)=Layer3; %integrate the sub-matrix S into the main matrix
+    
+    %Set up the part of the matrix at the interface with varying width
+    for l1=round(L_1/delta_x):round(L_1/delta_x)+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
+        A(l1,l1-1) = sigma_int1;
+        A(l1,l1) = 1-2*sigma_int1;
+        A(l1,l1+1) = sigma_int1;
+    end
+    
+    A(round(L_1/delta_x)+int_width,round(L_1/delta_x)+1+int_width) = sigma_2;
+    A(round(L_1/delta_x)+1+int_width,round(L_1/delta_x)+int_width) = sigma_int1; % Manual modification to ensure the matrix is set up correctly
+    
+    
+    %Set up the part of the matrix at the second interface with varying width
+    for l2=round((L_1+L_2)/delta_x):round((L_1+L_2)/delta_x)+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
+        A(l2,l2-1) = sigma_int2;
+        A(l2,l2) = 1-2*sigma_int2;
+        A(l2,l2+1) = sigma_int2;
+    end
+    
+    A(round((L_1+L_2)/delta_x)+int_width,round((L_2+L_1)/delta_x)+1+int_width) = sigma_3;
+    A(round((L_1+L_2)/delta_x)+1+int_width,round((L_2+L_1)/delta_x)+int_width) = sigma_int2; % Manual modification to ensure the matrix is set up correctly
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %Setting up the transformation matrix for the next timestep A(n+1), which
+    %is just the same as the previous matrix but with flipped signs.
+    
+    % Set up a sub-matrix L which contains the information regarding the surface exchange as well as diffusion through the LSCF layer
+    Layer1_n = full(gallery('tridiag',round(L_1/delta_x),-sigma_1,1+2*sigma_1,-sigma_1)); % Bulk diffusion parameters
+    Layer1_n(1,1) = 1+2*sigma_1*(1+delta_x*h); %Surface boundary condition
+    Layer1_n(1,2) = -2*sigma_1; %Surface boundary condition
+    
+    % Set up a sub-matrix E for the bulk diffusion properties of the electrolyte
+    Layer2_n = full(gallery('tridiag',round((L_2-delta_x)/delta_x+1),-sigma_2,1+2*sigma_2,-sigma_2)); %Bulk diffusion parameters
+    
+    % Set up a sub-matrix S for the bulk diffusion properties of the substrate
+    Layer3_n = full(gallery('tridiag',round((L_3-delta_x)/delta_x)+2,-sigma_3,1+2*sigma_3,-sigma_3)); %Bulk diffusion parameters
+    Layer3_n(end,end) = 1-sigma_3; %Mirror boundary condition parameters
+    Layer3_n(end,end-1) = 2*sigma_3;
+    Layer3_n(end,end-2) = -sigma_3;
+    
+    % Set up the main matrix which combines L, E, S and interface properties into one
+    A_n = full(gallery('tridiag',round(steps_x),-sigma_2,1+2*sigma_2,-sigma_2)); % Create a tridiagonal basis using the electrolyte values
+    A_n(1:round(L_1/delta_x),1:round(L_1/delta_x))=Layer1_n; %integrate the sub-matrix L into the main matrix
+    A_n(1+round(L_1/delta_x):round((L_2+L_1)/delta_x),1+round(L_1/delta_x):(round((L_2+L_1)/delta_x)))=Layer2_n; %integrate the sub-matrix E into the main matrix
+    A_n(round((Length-L_3)/delta_x)+1:end,round((Length-L_3)/delta_x)+1:end)=Layer3_n; %integrate the sub-matrix S into the main matrix
+    
+    %Set up the part of the matrix at the interface with varying width
+    for l1=round(L_1/delta_x):round(L_1/delta_x)+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
+        A_n(l1,l1-1) = -sigma_int1;
+        A_n(l1,l1) = 1+2*sigma_int1;
+        A_n(l1,l1+1) = -sigma_int1;
+    end
+    
+    A_n(round(L_1/delta_x)+int_width,round(L_1/delta_x)+1+int_width) = -sigma_2;
+    A_n(round(L_1/delta_x)+1+int_width,round(L_1/delta_x)+int_width) = -sigma_int1; % Manual modification to ensure the matrix is set up correctly
+    
+    
+    %Set up the part of the matrix at the second interface with varying width
+    for l2=round((L_2+L_1)/delta_x):round((L_2+L_1)/delta_x)+int_width % Creates a loop which ensures that the tridiagonal matrix at the interface is set up with varying interface width
+        A_n(l2,l2-1) = -sigma_int2;
+        A_n(l2,l2) = 1+2*sigma_int2;
+        A_n(l2,l2+1) = -sigma_int2;
+    end
+    
+    A_n(round((L_1+L_2)/delta_x)+int_width,round((L_2+L_1)/delta_x)+1+int_width) = -sigma_3;
+    A_n(round((L_1+L_2)/delta_x)+1+int_width,round((L_2+L_1)/delta_x)+int_width) = -sigma_int2; % Manual modification to ensure the matrix is set up correctly
+    
+    
 end
-
-A(L_1/delta_x+int_width,L_1/delta_x+1+int_width) = sigmaGDC;
-A(L_1/delta_x+1+int_width,L_1/delta_x+int_width) = sigma_int; % Manual modification to ensure the matrix is set up correctly
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%Setting up the transformation matrix for the next timestep A(n+1), which
-%is just the same as the previous matrix but with flipped signs.
-
-Layer1_n = full(gallery('tridiag',round(L_1/delta_x),-sigmaLSCF,1+2*sigmaLSCF,-sigmaLSCF));
-Layer1_n(1,1) = 1+2*sigmaLSCF*(1+delta_x*h);
-Layer1_n(1,2) = -2*sigmaLSCF;
-
-Layer2_n = full(gallery('tridiag',round((L_2-delta_x)/delta_x)+2,-sigmaGDC,1+2*sigmaGDC,-sigmaGDC));
-Layer2_n(end,end) = 1-sigmaGDC;
-Layer2_n(end,end-1) = 2*sigmaGDC;
-Layer2_n(end,end-2) = -sigmaGDC;
-
-A_n = full(gallery('tridiag',round(steps_x),-sigmaGDC,1+2*sigmaGDC,-sigmaGDC));
-A_n(1:L_1/delta_x,1:L_1/delta_x)=Layer1_n;
-A_n(round((Length-L_2)/delta_x)+1:end,round((Length-L_2)/delta_x)+1:end)=Layer2_n;
-
-for l=L_1/delta_x:(L_1+int_width)/delta_x
-    A_n(l,l-1) = -sigma_int;
-    A_n(l,l) = 1+2*sigma_int;
-    A_n(l,l+1) = -sigma_int;
-end
-
-A_n(L_1/delta_x+int_width,L_1/delta_x+1+int_width) = -sigmaGDC;
-A_n(L_1/delta_x+1+int_width,L_1/delta_x+int_width) = -sigma_int;
 
 % Define surface exchange vector
 G = zeros(round(steps_x), 1);
-G(1) = 4*delta_x*sigmaLSCF*h; %surface boundary condition vector G
+G(1) = 4*delta_x*sigma_1*h; %surface boundary condition vector G
 
 C = zeros(round(steps_x),1);%Initial position vector, 0 everywhere
 
@@ -90,9 +240,9 @@ C = zeros(round(steps_x),1);%Initial position vector, 0 everywhere
 
 for t=1:steps_t
     
-    C = A_n\(A*C+G);   
- 
-end
+    C = A_n\(A*C+G);
     
+end
+
 
 end
--- Interface/CNIntFitting.m
+++ Interface/CNIntFitting.m
@@ -1,29 +1,115 @@
-%% Getting the data file
+function [Dk,data,cn] = CNIntFitting(CNInt(Dk))
 
-[FileName, PathName] = uigetfile('*.*');
+%% Define global variables
+% This ensures that the following variables are the same across all
+% function files
+
+global steps_x;
 global data;
-data=transpose(textread([PathName,'\',FileName],'%n')');
+global L_1;
+global L_2;
+global L_3;
+global answer;
+global Duration;
+DeveloperMode = 0;
 
-%% Setting up fminsearch
+prompt = {'Enter length of the profile in micrometers','Enter number of layers','Experiment time in h'}; % Creates a pop-up window that asks the user to enter the physical length of the diffusion profile in microns
+    dlgtitle = 'Input';
+    dims = 1;
+    definput = {'2.4','2','1'};
+    answer = inputdlg(prompt,dlgtitle,dims,definput);
+    Duration = str2double(answer(3)); % Defines the experiment time
 
-fun = @(Dk) sum((data-CNInt(Dk)).^2); % set up the sum squared function
-
-Dk0 = [0.009,0.264,0.00100,0.0017]; % Initial guesses for DLSCF, DGDC, DINT and k in that order
-
-
-options = optimset('MaxIter',2000,'MaxFunEvals',3000, 'TolFun',1e-8,'TolX',1e-8); % set options for fminsearch
-[Dk, fminres] = fminsearch(fun,Dk0,options); % the actual fminsearch
-
-disp(['DLSCF = ',num2str(Dk(1)),'  DGDC = ',num2str(Dk(2)),' DINT = ',num2str(Dk(3)),' k = ',num2str(Dk(4))]); % display the output variables
-
-writematrix(Dk,'FittedValues.xlsx') % Write the values out onto an Excel file which can then be further processed
-
+if DeveloperMode == 0
+    %% Getting the data file
+    
+    [FileName, PathName] = uigetfile('*.*'); % Call a pop-up UI feature that allows the user to select data files
+    data=transpose(textread([PathName,'\',FileName],'%n')'); % Reads the data file
+    
+    %% Extracting information from the data file
+    
+    if str2double(answer(2))== 1 % If there is only one single material
+        
+        scale = (length(data)-1)/str2double(answer(1));
+        L_1 = length(data)/scale;
+        L_2 = 0;
+        L_3 = 0;
+        
+    elseif str2double(answer(2)) == 2 % Set the case for two layers and only picking one data point
+        
+        plot(data)
+        [x] = ginput(1); % Asks the user to select a point on the graph to mark the interface location, then extracts the x value of that point
+        scale = (length(data)-1)/str2double(answer(1)); % Sets a scaling factor based on the user input to allow conversion of the x values from the datafile into real lengths
+        L_1 = x(1)/scale; % Determines the length of the first layer based on user input
+        L_2 = (length(data)-1)/scale-L_1; % Determines the length of the second layer
+        L_3 = 0;
+        close
+        
+    elseif str2double(answer(2)) == 3 % Set the case for three layers and picking two data points
+        
+        plot(data)
+        [x] = ginput(2); % Asks the user to select a point on the graph to mark the interface location, then extracts the x value of that point
+        scale = (length(data)-1)/str2double(answer(1));
+        L_1 = x(1)/scale;
+        L_2 = x(2)/scale;
+        L_3 = (length(data)-1)/scale-(L_1+L_2);
+        close
+    end
+    
+    %% Setting up and running fminsearch
+    
+    fun = @(Dk) sum((data-CNInt(Dk)).^2); % set up the sum squared function
+    
+    if str2double(answer(2)) == 1 % Condition for a single material
+        Dk0 = [0.1,0.1]; % D and k
+    elseif    str2double(answer(2)) == 2 % Ensures that 4 variables are run in fminsearch
+        Dk0 = [0.009,0.264,0.00100,0.0017]; % Initial guesses for DLSCF, DGDC, DINT and k in that order
+    elseif str2double(answer(2)) == 3 % Ensures that 6 variables are run
+        Dk0 = [0.01,0.01,0.1,0.01,1,0.001]; %Initial guesses for D1, D2, D3, DINT1, DINT 2 and k in that order
+    end
+    
+    options = optimset('MaxIter',2000,'MaxFunEvals',3000, 'TolFun',1e-8,'TolX',1e-8); % set options for fminsearch
+    [Dk, fminres] = fminsearch(fun,Dk0,options); % the actual fminsearch
+    
+    % Change the output depending on how many variables are fitted
+    
+    if str2double(answer(2)) == 1
+        disp(['D1 = ',num2str(Dk(1)),' k = ',num2str(Dk(2))]);
+    elseif str2double(answer(2)) == 2
+        disp(['D1 = ',num2str(Dk(1)),'  D2 = ',num2str(Dk(2)),' DINT = ',num2str(Dk(3)),' k = ',num2str(Dk(4))]); % display the output variables
+    elseif str2double(answer(2)) == 3
+        disp(['D1 = ',num2str(Dk(1)),'  D2 = ',num2str(Dk(2)),' D3 = ',num2str(Dk(3)),' DINT1 = ',num2str(Dk(4)),' DINT2 = ',num2str(Dk(5)),' k = ',num2str(Dk(6))]); % display the output variables
+    end
+    
+    writematrix(Dk,'FittedValues.xlsx') % Write the values out onto an Excel file which can then be further processed
+    
+elseif DeveloperMode == 1
+    
+    data=load('DBLayerProfile.txt');
+    L_1 = 0.2;
+    L_2 = 0.8;
+    L_3 = 0;
+    
+    fun = @(Dk) sum((data-CNInt(Dk)).^2);
+    
+    Dk0 = [0.009,0.264,0.00100,0.0017]; %Initial guesses for DLSCF, DGDC, DINT and k in that order
+    
+    
+    options = optimset('MaxFunEvals',2000, 'TolFun',1e-6,'TolX',1e-6);
+    [Dk, fminres] = fminsearch(fun,Dk0,options);
+    
+    disp(['DLSCF = ',num2str(Dk(1)),'  DGDC = ',num2str(Dk(2)),' DINT = ',num2str(Dk(3)),' k = ',num2str(Dk(4))]);
+    
+end
 %% plotting
 
-cn=CNInt(Dk);
-plot(cn)
+cn=CNInt(Dk); % Calls the final output from the fitting algorithm
+x=linspace(0,L_1+L_2+L_3,steps_x); % Set up x values in accordance with the real scale of the data
+plot(x,cn)
 hold on
-plot(data, '.')
+plot(x,data, '.')
 legend('Fit','Data')
 xlabel('Depth / um')
 ylabel('Isotopic Fraction')
+
+end
